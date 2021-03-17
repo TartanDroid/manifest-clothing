b9631808f2344b0ec22ebb0ce289ac515d620369
Gary Watson <gary.watson@tartandroid.com>
2021-03-17 09:45:38 +0000

    cart icon and cart dropdown and toggle were added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo"></Logo>
@@ -18,17 +20,20 @@ const Header = ({ currentUser }) => (
                 CONTACT
             </Link>
             {
-                currentUser ?
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className="option" to="/signin">SIGN IN</Link>
-            }
+            currentUser ? (
+            <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+            ):(
+            <Link className="option" to="/signin">SIGN IN</Link>
+            )}
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: {currentUser}, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)(Header);
